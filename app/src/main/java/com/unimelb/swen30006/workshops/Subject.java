3ab44093b39c331edb3f80457ed776e7cf6e38b0
Hongyuee <hongyuel1@student.unimelb.edu.au>
2025-03-25 15:36:12 +1100

    update

---
+++ app/src/main/java/com/unimelb/swen30006/workshops/Subject.java
@@ -0,0 +1,12 @@
+package com.unimelb.swen30006.workshops;
+
+public class Subject {
+    private String name;
+    private String subjectCode;
+    private List<Assignment> Assignments;
+
+    
+
+    public void createAssignment() {}
+    public void deleteAssignment(String name) {}
+}
